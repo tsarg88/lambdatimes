ff588903188b322dc44774fd63c648476fd38da8
umbur <work1415@gmail.com>
2019-02-09 17:47:07 -0700

    files for this project

---
+++ src/components/Content/Card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const Card = props => {
+  //console.log('card.js:',props)
+  return (
+    <div className="card">
+      <div className="headline">{props.cards.headline}</div>
+      <div className="author">
+        <div className="img-container">
+          <img src={props.cards.img}
+          alt='card' />
+        </div>
+        <span>By {props.cards.author}</span>
+      </div>
+    </div>
+  );
+};
+
+// Make sure to include PropTypes.
+Card.propTypes = {
+  Cards: PropTypes.shape({
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+  })
+}
+
+export default Card;
